cf2016bda88b0866640ce2b08cc0f1214c51ae9e
Huyen Trang Dinh <tran9dinh@gmail.com>
2018-02-27 22:11:12 -0500

    fini, reste juste division, sorry tu pourras pratiquer mais c du hassle

--- inf1600_tp3/average.s
+++ inf1600_tp3/average.s
@@ -1,11 +1,82 @@
 .global matrix_row_aver_asm
 
 matrix_row_aver_asm:
+
         push %ebp      			/* Save old base pointer */
         mov %esp, %ebp 			/* Set ebp to current esp */
 
-		/* Write your solution here */
+
+        /*saving live registers/
+        pushl %esi
+        pushl %edi
+        
+       	/* Making room for r, c and elem*/
+        subl $12, %esp
+        movl $0, -12(%ebp)			#r
+        movl $0, -16(%ebp)			#c
+        
+        jmp condr
+        
+        
+        		
+
+
+   for(r = 0; r < matorder; ++r) {
+	  elem = 0;
+      for(c = 0; c < matorder; ++c) {
+         elem += inmatdata[c + r * matorder];         
+      }
+	  outmatdata[r] = elem/matorder;
+   }
+   
+boucle:
 		
+		/*indice dans ecx*/
+		movl -12(%ebp), %ecx				#r
+		imull 16(%ebp), %ecx				#matorder
+		addl -16(%ebp), %ecx				#c
+		
+		movl 8(%ebp), %esi					#inmatdata
+		movl (%esi, %ecx, 4), %eax			#inmatdata[c + r * matorder] dans eax
+		addl %eax, -20(%ebp)				# elem =+ eax
+
+incc:
+
+		addl $1, -16(%ebp)
+       
+condc:
+
+		movl -16(%ebp), %eax
+		movl 16(%ebp), %edx
+		cmp %eax, %edx
+		ja boucle
+		movl $0, -16(%ebp)					#remise de c a 0
+		
+		movl -12(%ebp), %ecx				#r
+		movl -20(%ebp), %eax				#elem
+		movl 16($ebp), %edx					#matorder
+		############DIVISION elem/matorder ?#################
+		movl 12(%ebp), %edi					#outmatdata
+		movl DIVISION!?!??, (%edi, %ecx, 4)	# outmatdata[r] = elem/matorder
+		
+		
+incr:
+
+		addl $1, -12(%ebp)
+
+condr:
+
+		movl -12(%ebp), %eax
+		movl 16(%ebp), %edx
+		cmp %eax, %edx				
+		movl $0, -20(%ebp)					#elem = 0
+		ja condc
+        
+		/* aucun retour, rien a mettre dans %eax */
+		/* delete local vars */
+		addl $12, %esp
+		/* recovering registers */
+		pop %edi
+		pop %esi
         leave          			/* Restore ebp and esp */
         ret           			/* Return to the caller */
-		
--- inf1600_tp3/multiply.s
+++ inf1600_tp3/multiply.s
@@ -1,86 +1,98 @@
 .globl matrix_multiply_asm
 
 matrix_multiply_asm:
+
      		push %ebp      			/* Save old base pointer */
         	mov %esp, %ebp 			/* Set ebp to current esp */
 
 
         	/*Saving live registers selon gcc calling conventions*/
        		pushl %edi
-		pushl %esi
-		pushl %ebx
+			pushl %esi
+			pushl %ebx
         
        		/* Making room for r and c and initialize (local vars)*/
         	subl $16, %esp
-        	movl $0, -12(%ebp)			#r
-        	movl $0, -16(%ebp)			#c
-        	movl $0, -20(%ebp)			#i
+        	movl $0, -16(%ebp)			#r
+        	movl $0, -20(%ebp)			#c
+        	movl $0, -24(%ebp)			#i
 
         	jmp condr
-   
+        	
+bouclec:
+	
+			movl $0, -28(%ebp)			#elem = 0
+			jmp condi
+			
+bouclei:
 
-
-
-/*    for(r = 0; r < matorder; ++r) { */
-/*       for(c = 0; c < matorder; ++c) { */
-/*          elem = 0; */
-/*          for(i = 0; i < matorder; ++i) */
-/*             elem += inmatdata1[i + r * matorder] * inmatdata2[c + i * matorder]; */
-/*          outmatdata[c + r * matorder] = elem; */
-/*       } */
-/*    } */
-
-boucle:
-
-		movl $0, -24(%ebp)				#elem = 0 
-
-		/*valeur indexation dans ecx : PREMIER TERME */
-		movl -12(%ebp), %ecx				# r dans ecx
-		imul 16(%ebp), %ecx				# ecx * matorder
-		addl -20(%ebp), %ecx				# ecx + i
+			/*valeur indexation dans ecx : PREMIER TERME */
+			movl -16(%ebp), %ecx				# r dans ecx
+			imull 20(%ebp), %ecx				# ecx * matorder
+			addl -24(%ebp), %ecx				# ecx + i
 		
-		movl 8(%ebp), %esi
-		movl (%esi, %ecx, 4),%eax			# j'ai le premier terme dans eax
+			movl 8(%ebp), %esi					# in1 dans esi
+			movl (%esi, %ecx, 4),%eax			# j'ai le premier terme dans eax
+		
+			/* DEUXIEME TERME */
+			movl -24(%ebp), %ecx				# i dans ecx
+			imull 20(%ebp), %ecx				# ecx * matorder
+			addl -20(%ebp), %ecx				# ecx + c
+			movl 12(%ebp), %edi					# in2 dans edi
+			imull (%edi, %ecx, 4), %eax			# multiplication des deux termes et stores dans %eax
+			
+			addl %eax ,-28(%ebp)				# additionne eax et elem et met le res dans elem (elem += eax)
 
-		
-		
-		jmp incc
-		
-else:
-		/*affection par 0*/
-		movl -12(%ebp), %ecx				# r dans ecx
-		imul 16(%ebp), %ecx				# ecx * matorder
-		addl -16(%ebp), %ecx				# ecx + c
-		
-		movl 12(%ebp), %esi
-		movl $0, (%esi, %ecx, 4)
+inci:
 
+			addl $1, -24(%ebp)
+			
+condi:
+
+			movl -24(%ebp), %eax
+			movl 20(%ebp), %edx
+			cmp %eax, %edx
+			ja bouclei							# revient a la boucle de i
+			movl $0, -24(%ebp)					# remise de i a 0
+			
+			movl -16(%ebp), %ecx				
+			imul 20(%ebp), %ecx
+			addl -20(%ebp), %ecx				# c + r*matorder dans %ecx
+			
+			movl 16(%ebp), %esi					# outmatdata dans esi
+			movl -28(%ebp), %eax
+			movl %eax, (%esi, %ecx, 4)			# outmatdata[c + r * matorder] = elem
+			
+			
 incc:
-
-		addl $1, -16(%ebp)
-       
+			addl $1, -20(%ebp)
 condc:
 
-		movl -16(%ebp), %eax
-		movl 16(%ebp), %edx
-		cmp %eax, %edx
-		ja boucle
-		movl $0, -16(%ebp)
+			movl -20(%ebp), %eax
+			movl 20(%ebp), %edx
+			cmp %eax, %edx
+			movl $0, -24(%ebp)					#elem = 0 
+			ja bouclec
+			movl $0, -20(%ebp)					#remise de c 0
+			
 incr:
 
-		addl $1, -12(%ebp)
+			addl $1, -16(%ebp)
 
 condr:
 
-		movl -12(%ebp), %eax
-		movl 16(%ebp), %edx
-		cmp %eax, %edx
-		ja condc
-        
-		/* aucun retour, rien a mettre dans %eax */
-		addl $16, %esp
-		
-		pop %esi
-		pop %edi
+			movl -16(%ebp), %eax
+			movl 20(%ebp), %edx
+			cmp %eax, %edx
+			ja condc
+
+
+			/* aucun retour, rien a mettre dans %eax */
+			
+			addl $16, %esp
+			pop %ebx							#on ne s'est pas servies de ebx, mais on aura au moin
+			pop %esi							#suivi les calling conventions de gcc
+			pop %edi
+			
       		leave          /* restore ebp and esp */
         	ret            /* return to the caller */
